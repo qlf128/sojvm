cc2cbb1a3ff74a98cb226a5e378e65444aca6873
windpursuer <1059641703@qq.com>
2018-12-27 20:44:18 +0800

    恢复错误信息

--- src/main/java/com/jvm/nativeLoader/NativeLibraryLoad.java
+++ src/main/java/com/jvm/nativeLoader/NativeLibraryLoad.java
@@ -172,8 +172,7 @@ public class NativeLibraryLoad {
             //action
             PrivilegedAction privilegedAction = new PrivilegedAction<Object>() {
                 public Object run() {
-                    //return libFile.exists() ? Boolean.TRUE : null;
-                    return null;
+                    return libFile.exists() ? Boolean.TRUE : null;
                 }};
             boolean exists = AccessController.doPrivileged(privilegedAction) != null;
             String name = null;
